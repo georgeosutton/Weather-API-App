6055cfbf38db3a6c3f0e4dd45aa3ec80d1592893
georgeosutton <46813782+georgeosutton@users.noreply.github.com>
2020-09-18 20:40:26 +0100

    Update app.js

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ if (navigator.geolocation){
 long = position.coords.longitude;
 lat = position.coords.latitude;
 
-const api = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&appid=28def05e60bacf29656e604ca4b2be85`;
+const api = `api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&appid=28def05e60bacf29656e604ca4b2be85`;
 
 
 fetch(api)
