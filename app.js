dcd43ba60c2d492df3419424cec43d90ce24132e
georgeosutton <46813782+georgeosutton@users.noreply.github.com>
2021-01-04 23:57:18 +0000

    Update app.js

--- app.js
+++ app.js
@@ -1,61 +1,49 @@
-let tempDesc = document.querySelector('.temperature-description');
-let tempDegree = document.querySelector('.temperature-degree');
-let Currentlocation = document.querySelector('.location-h1');
-let weatherIcon = document.querySelector('.weather-icon')
+let tempDesc = document.querySelector(".temperature-description");
+let tempDegree = document.querySelector(".temperature-degree");
+let Currentlocation = document.querySelector(".location-h1");
+let weatherIcon = document.querySelector(".weather-icon");
 
-window.addEventListener('load', ()=>{
-let long;
-let lat;
+window.addEventListener("load", () => {
+  let long;
+  let lat;
 
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition((position) => {
+      long = position.coords.longitude;
+      lat = position.coords.latitude;
 
-if (navigator.geolocation){
-     navigator.geolocation.getCurrentPosition(position =>{
+      const api = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&appid=${process.env.APP_WEATHER_API}`;
 
-long = position.coords.longitude;
-lat = position.coords.latitude;
+      fetch(api)
+        .then((response) => {
+          return response.json();
+        })
+        .then((data) => {
+          console.log(data);
+          let { temp } = data.main;
 
-const api = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&appid=b72067057be53cb5c1a6f0ab3416641b`;
+          temp = Math.round(temp - 273.15);
+          tempDegree.textContent = temp;
 
+          let name = data.name;
+          Currentlocation.textContent = name;
 
-fetch(api)
-     .then(response =>{
-         return response.json();
-     })
-     .then(data =>{
-         console.log(data)
-         let {temp} = data.main;
-        
-         temp = Math.round(temp - 273.15);
-         tempDegree.textContent = temp;
+          let { icon, description } = data.weather["0"];
 
-
-
-         let name = data.name;
-         Currentlocation.textContent = name;
-
-
-
-
-         let {icon, description} = data.weather["0"];
-         
-         setIcon(icon)
-         setDesciption(description)
-     });
+          setIcon(icon);
+          setDesciption(description);
+        });
     });
-} 
+  }
 
-function setIcon(icon){
-    weatherIcon.src = `http://openweathermap.org/img/wn/${icon}@2x.png`
-    console.log(icon)
-}
+  function setIcon(icon) {
+    weatherIcon.src = `http://openweathermap.org/img/wn/${icon}@2x.png`;
+    console.log(icon);
+  }
 
-function setDesciption(description){
-    tempDesc.textContent= description.replace(/\w\S*/g, (w) => (w.replace(/^\w/, (c) => c.toUpperCase())));
-}
-
+  function setDesciption(description) {
+    tempDesc.textContent = description.replace(/\w\S*/g, (w) =>
+      w.replace(/^\w/, (c) => c.toUpperCase())
+    );
+  }
 });
-
-
-
-
-
